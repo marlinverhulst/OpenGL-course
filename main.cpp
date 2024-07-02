0e9d952b3105af28d3ab60759b78927191b237c6
marlinverhulst <marlinverhulst@gmail.com>
2024-07-02 20:40:06 +0200

    init opengl

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 
 int main(void)
@@ -10,10 +11,15 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+     window = glfwCreateWindow(1200, 800, "OpenGL 3.3", NULL, NULL);
     if (!window)
     {
+        std::cout << "Failed to create window\n";
         glfwTerminate();
         return -1;
     }
@@ -21,12 +27,19 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    if (glewInit() != GLEW_OK);
+    if (glewInit() != GLEW_OK) {
+        std::cout << "failed to init GLEW\n";
+        glfwTerminate();
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Update here*/
+
+
         /* Render here */
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
@@ -36,6 +49,9 @@ int main(void)
         glfwPollEvents();
     }
 
+
+    // CLEARING
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
